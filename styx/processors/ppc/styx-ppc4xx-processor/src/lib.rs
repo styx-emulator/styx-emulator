yuv418/fork-styx-emulator

feat: multi-proc, upgrade styx-ppc4xx-processor
The most notable change is the removal of post_irq_route_hook.
I tested with the ppc405-freertos-demo and it seems to work
perfectly fine.

styx/processors/ppc/styx-ppc4xx-processor/src/lib.rs
@@ -75,32 +75,32 @@ impl ProcessorImpl for PowerPC405Builder {
                 .context("ppc405 processor only supports pcode backend");
         };
 
-        let tlb = Box::new(tlb::Ppc405Tlb::new());
+        let tlb = tlb::Ppc405Tlb::new();
         let mut memory = MemoryBackend::new_region_store();
 
         memory.memory_map(0, 2u64.pow(32), MemoryPermissions::all())?;
 
         self.initial_registers(cpu.as_mut())?;
-        let cec = Box::new(CoreEventController::new(cpu.as_mut(), args.runtime.clone()));
 
-        let mut peripherals: Vec<Box<dyn Peripheral>> = Vec::new();
-        let timers = Box::new(Timers::new(cpu.as_mut()));
-        peripherals.push(timers);
+        let cec = CoreEventController::new(cpu.as_mut(), args.runtime.clone());
+
+        // Construct peripherals while the cpu is still available.
+        // Timers specifically initialize registers.
+        // TODO: this should probably be in the peripheral init.
+        let timers = Timers::new(cpu.as_mut());
         let uart = UartController::new(vec![UartInterface::new("0".into(), NewUartPortInner)]);
-        peripherals.push(Box::new(uart));
         let ethernet = EthernetController::new();
-        peripherals.push(Box::new(ethernet));
 
-        let mut hints = LoaderHints::new();
-        hints.insert("arch".to_string().into_boxed_str(), Box::new(Arch::Ppc32));
-        Ok(ProcessorBundle {
-            cpu,
-            tlb,
-            memory,
-            event_controller: cec,
-            peripherals,
-            loader_hints: hints,
-        })
+        Ok(ProcessorBundle::builder()
+            .with_memory(memory)
+            .with_vcpu(|v| v.with_cpu_box(cpu).with_tlb(tlb).with_event_controller(cec))
+            // single vcpu, no custom event distributor implementation needed
+            .with_event_distributor(SingleVcpuEventController::default())
+            .add_peripheral(timers)
+            .add_peripheral(uart)
+            .add_peripheral(ethernet)
+            .with_arch_hint(Arch::Ppc32)
+            .build()?)
     }
 }
 
@@ -139,37 +139,38 @@ mod tests {
             .build()
             .unwrap();
 
-        proc.core.mmu.code().write(0).bytes(input_bytes).unwrap();
+        proc.memory().code().write(0).bytes(input_bytes).unwrap();
         // Add a code hook to automatically stop when we hit the infinite loop
         // at the end of the benchmark.
-        proc.add_hook(StyxHook::code(0..=0xffffffff, |cpu: CoreHandle| {
-            // Either of these are probable "infinite loop" instructions
-            let stop_instructions = [
-                0x4bfffffcu32, // b pc-1
-                0x48000000u32, // b pc ; generated by -O3
-            ]
-            .map(|instr| instr.to_be_bytes());
-            let mut stop_instructions = stop_instructions.iter().map(|bytes| bytes.as_slice());
+        proc.vcpus[0]
+            .cpu
+            .add_hook(StyxHook::code(.., |cpu: CoreHandle| {
+                // Either of these are probable "infinite loop" instructions
+                let stop_instructions = [
+                    0x4bfffffcu32, // b pc-1
+                    0x48000000u32, // b pc ; generated by -O3
+                ]
+                .map(|instr| instr.to_be_bytes());
+                let mut stop_instructions = stop_instructions.iter().map(|bytes| bytes.as_slice());
 
-            let pc = cpu.cpu.pc().unwrap();
-            let current_instruction = cpu.mmu.data().read(pc).vec(4).unwrap();
-            if stop_instructions.any(|instr| instr == current_instruction) {
-                debug!("Found infinite loop, stopping");
-                cpu.cpu.stop();
-            }
-            Ok(())
-        }))
-        .unwrap();
+                let pc = cpu.cpu.pc().unwrap();
+                let current_instruction = cpu.mmu.data().read(pc).vec(4).unwrap();
+                if stop_instructions.any(|instr| instr == current_instruction) {
+                    debug!("Found infinite loop, stopping");
+                    cpu.cpu.stop();
+                }
+                Ok(())
+            }))
+            .unwrap();
 
-        proc.core.cpu.set_pc(INSTRUCTION_TEST_START).unwrap();
+        proc.vcpus[0].cpu.set_pc(INSTRUCTION_TEST_START).unwrap();
 
         // run firmware
         proc.run(Forever).unwrap();
 
         // check final result value stored in r15
         // must be changed if more tests are added/removed
-        let result = proc
-            .core
+        let result = proc.vcpus[0]
             .cpu
             .read_register::<u32>(Ppc32Register::R15)
             .unwrap();
@@ -177,8 +178,7 @@ mod tests {
 
         // check SPR 0x3DA (TCR)
         // this is set at end of firmware
-        let tcr_value = proc
-            .core
+        let tcr_value = proc.vcpus[0]
             .cpu
             .read_register::<u32>(SpecialPpc32Register::SprRegister(
                 SprRegister::new(0x3DA).unwrap(),
@@ -188,8 +188,7 @@ mod tests {
             tcr_value, 0x1337,
             "tcr_value is wrong value 0x{tcr_value:b}!"
         );
-        let tcr_value = proc
-            .core
+        let tcr_value = proc.vcpus[0]
             .cpu
             .read_register::<u32>(Ppc32Register::Tcr)
             .unwrap();
@@ -212,8 +211,8 @@ mod tests {
         for i in 0x150..0x200 {
             let spr_reg = SpecialPpc32Register::SprRegister(SprRegister::new(i).unwrap());
             let value = (i % 0x13) as u32;
-            proc.core.cpu.write_register(spr_reg, value).unwrap();
-            let read_value = proc.core.cpu.read_register::<u32>(spr_reg).unwrap();
+            proc.vcpus[0].cpu.write_register(spr_reg, value).unwrap();
+            let read_value = proc.vcpus[0].cpu.read_register::<u32>(spr_reg).unwrap();
             assert_eq!(
                 value, read_value,
                 "incorrect value read back 0x{read_value:x} != 0x{value:x}"
@@ -234,26 +233,24 @@ mod tests {
             .with_builder(PowerPC405Builder::default())
             .build()
             .unwrap();
-        proc.core.mmu.code().write(0x0).bytes(&input_bytes).unwrap();
+        proc.memory().code().write(0x0).bytes(&input_bytes).unwrap();
 
-        proc.core.cpu.set_pc(0).unwrap();
-        proc.core
+        proc.vcpus[0].cpu.set_pc(0).unwrap();
+        proc.vcpus[0]
             .cpu
             .write_register(Ppc32Register::R4, 0u32)
             .unwrap();
-        proc.core
+        proc.vcpus[0]
             .cpu
             .write_register(Ppc32Register::R9, 0u32)
             .unwrap();
         proc.run(2).unwrap();
-        let r4 = proc
-            .core
+        let r4 = proc.vcpus[0]
             .cpu
             .read_register::<u32>(Ppc32Register::R4)
             .unwrap();
         assert_eq!(4, r4);
-        let r4 = proc
-            .core
+        let r4 = proc.vcpus[0]
             .cpu
             .read_register::<u32>(Ppc32Register::R9)
             .unwrap();

styx-emulator/styx-emulator

styx/processors/ppc/styx-ppc4xx-processor/src/lib.rs
@@ -75,32 +75,29 @@ impl ProcessorImpl for PowerPC405Builder {
                 .context("ppc405 processor only supports pcode backend");
         };
 
-        let tlb = Box::new(tlb::Ppc405Tlb::new());
+        let tlb = tlb::Ppc405Tlb::new();
         let mut memory = MemoryBackend::new_region_store();
 
         memory.memory_map(0, 2u64.pow(32), MemoryPermissions::all())?;
 
         self.initial_registers(cpu.as_mut())?;
-        let cec = Box::new(CoreEventController::new(cpu.as_mut(), args.runtime.clone()));
 
-        let mut peripherals: Vec<Box<dyn Peripheral>> = Vec::new();
-        let timers = Box::new(Timers::new(cpu.as_mut()));
-        peripherals.push(timers);
+        let cec = CoreEventController::new(cpu.as_mut(), args.runtime.clone());
+
+        let timers = Timers;
         let uart = UartController::new(vec![UartInterface::new("0".into(), NewUartPortInner)]);
-        peripherals.push(Box::new(uart));
         let ethernet = EthernetController::new();
-        peripherals.push(Box::new(ethernet));
 
-        let mut hints = LoaderHints::new();
-        hints.insert("arch".to_string().into_boxed_str(), Box::new(Arch::Ppc32));
-        Ok(ProcessorBundle {
-            cpu,
-            tlb,
-            memory,
-            event_controller: cec,
-            peripherals,
-            loader_hints: hints,
-        })
+        Ok(ProcessorBundle::builder()
+            .with_memory(memory)
+            .with_vcpu(|v| v.with_cpu_box(cpu).with_tlb(tlb).with_event_controller(cec))
+            // single vcpu, no custom event distributor implementation needed
+            .with_event_distributor(SingleVcpuEventDistributor::default())
+            .add_peripheral(timers)
+            .add_peripheral(uart)
+            .add_peripheral(ethernet)
+            .with_arch_hint(Arch::Ppc32)
+            .build()?)
     }
 }
 
@@ -139,37 +136,38 @@ mod tests {
             .build()
             .unwrap();
 
-        proc.core.mmu.code().write(0).bytes(input_bytes).unwrap();
+        proc.memory().code().write(0).bytes(input_bytes).unwrap();
         // Add a code hook to automatically stop when we hit the infinite loop
         // at the end of the benchmark.
-        proc.add_hook(StyxHook::code(0..=0xffffffff, |cpu: CoreHandle| {
-            // Either of these are probable "infinite loop" instructions
-            let stop_instructions = [
-                0x4bfffffcu32, // b pc-1
-                0x48000000u32, // b pc ; generated by -O3
-            ]
-            .map(|instr| instr.to_be_bytes());
-            let mut stop_instructions = stop_instructions.iter().map(|bytes| bytes.as_slice());
+        proc.vcpus[0]
+            .cpu
+            .add_hook(StyxHook::code(.., |cpu: CoreHandle| {
+                // Either of these are probable "infinite loop" instructions
+                let stop_instructions = [
+                    0x4bfffffcu32, // b pc-1
+                    0x48000000u32, // b pc ; generated by -O3
+                ]
+                .map(|instr| instr.to_be_bytes());
+                let mut stop_instructions = stop_instructions.iter().map(|bytes| bytes.as_slice());
 
-            let pc = cpu.cpu.pc().unwrap();
-            let current_instruction = cpu.mmu.data().read(pc).vec(4).unwrap();
-            if stop_instructions.any(|instr| instr == current_instruction) {
-                debug!("Found infinite loop, stopping");
-                cpu.cpu.stop();
-            }
-            Ok(())
-        }))
-        .unwrap();
+                let pc = cpu.cpu.pc().unwrap();
+                let current_instruction = cpu.mmu.data().read(pc).vec(4).unwrap();
+                if stop_instructions.any(|instr| instr == current_instruction) {
+                    debug!("Found infinite loop, stopping");
+                    cpu.cpu.stop();
+                }
+                Ok(())
+            }))
+            .unwrap();
 
-        proc.core.cpu.set_pc(INSTRUCTION_TEST_START).unwrap();
+        proc.vcpus[0].cpu.set_pc(INSTRUCTION_TEST_START).unwrap();
 
         // run firmware
         proc.run(Forever).unwrap();
 
         // check final result value stored in r15
         // must be changed if more tests are added/removed
-        let result = proc
-            .core
+        let result = proc.vcpus[0]
             .cpu
             .read_register::<u32>(Ppc32Register::R15)
             .unwrap();
@@ -177,8 +175,7 @@ mod tests {
 
         // check SPR 0x3DA (TCR)
         // this is set at end of firmware
-        let tcr_value = proc
-            .core
+        let tcr_value = proc.vcpus[0]
             .cpu
             .read_register::<u32>(SpecialPpc32Register::SprRegister(
                 SprRegister::new(0x3DA).unwrap(),
@@ -188,8 +185,7 @@ mod tests {
             tcr_value, 0x1337,
             "tcr_value is wrong value 0x{tcr_value:b}!"
         );
-        let tcr_value = proc
-            .core
+        let tcr_value = proc.vcpus[0]
             .cpu
             .read_register::<u32>(Ppc32Register::Tcr)
             .unwrap();
@@ -212,8 +208,8 @@ mod tests {
         for i in 0x150..0x200 {
             let spr_reg = SpecialPpc32Register::SprRegister(SprRegister::new(i).unwrap());
             let value = (i % 0x13) as u32;
-            proc.core.cpu.write_register(spr_reg, value).unwrap();
-            let read_value = proc.core.cpu.read_register::<u32>(spr_reg).unwrap();
+            proc.vcpus[0].cpu.write_register(spr_reg, value).unwrap();
+            let read_value = proc.vcpus[0].cpu.read_register::<u32>(spr_reg).unwrap();
             assert_eq!(
                 value, read_value,
                 "incorrect value read back 0x{read_value:x} != 0x{value:x}"
@@ -234,26 +230,24 @@ mod tests {
             .with_builder(PowerPC405Builder::default())
             .build()
             .unwrap();
-        proc.core.mmu.code().write(0x0).bytes(&input_bytes).unwrap();
+        proc.memory().code().write(0x0).bytes(&input_bytes).unwrap();
 
-        proc.core.cpu.set_pc(0).unwrap();
-        proc.core
+        proc.vcpus[0].cpu.set_pc(0).unwrap();
+        proc.vcpus[0]
             .cpu
             .write_register(Ppc32Register::R4, 0u32)
             .unwrap();
-        proc.core
+        proc.vcpus[0]
             .cpu
             .write_register(Ppc32Register::R9, 0u32)
             .unwrap();
         proc.run(2).unwrap();
-        let r4 = proc
-            .core
+        let r4 = proc.vcpus[0]
             .cpu
             .read_register::<u32>(Ppc32Register::R4)
             .unwrap();
         assert_eq!(4, r4);
-        let r4 = proc
-            .core
+        let r4 = proc.vcpus[0]
             .cpu
             .read_register::<u32>(Ppc32Register::R9)
             .unwrap();